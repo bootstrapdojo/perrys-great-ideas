4719ee08b08a2b12996e25a92b944201b28c2707
a b <a b>
2017-04-22 03:27:26 +0800

    DI

--- app.js
+++ app.js
@@ -2,13 +2,16 @@
 var greatIdeasApp = angular.module('greatIdeasApp', ['ngResource']);
 
 // Define the `IdeasListController` controller on the `greatIdeasApp` module
-greatIdeasApp.controller('IdeasListController', function IdeasListController($scope, $resource) {
+greatIdeasApp.controller('IdeasListController', function IdeasListController($scope, ideasResource) {
 
-    var resource = $resource('https://api.myjson.com/bins/1gxu7j');
-    var ideasResponse = resource.get();
+    var ideasResponse = ideasResource.get();
 
     ideasResponse.$promise.then(function (data) {
         $scope.ideas = data.ideas;
     });
 });
 
+greatIdeasApp.factory('ideasResource', function ($resource) {
+    var ideasResource = $resource('https://api.myjson.com/bins/1gxu7j');
+    return ideasResource;
+});
